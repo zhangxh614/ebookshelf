89c80810f4d2606e6de552b7965a47902419e1ab
zhangxh614 <zhangxh614@gmail.com>
2017-07-26 14:58:51 +0800

    json

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -29,6 +29,8 @@ $(function() {
 			var self = this;
 
 			document.getElementById("picture").addEventListener("click", function(e) {
+				canvas.width = video.offsetWidth;
+				canvas.height = video.offsetHeight;
 				context.drawImage(video, 0, 0);
 				imgData = canvas.toDataURL('image/png');
 				console.log(tmp === imgData);
@@ -54,11 +56,16 @@ $(function() {
 			});
 			Quagga.decodeSingle(config, function(result) {
 				if (result === undefined) {
-					alert("not code");
+					alert("Not code in the picture.");
 				} else if (result.codeResult) {
-					alert("result: " + String(result.codeResult.code));
+					let json = '{\"ISBN\":\"' + result.codeResult.code + '\"}';
+					alert(json);
+					$.ajax({
+						'url': '\\crawler',
+						'type': 'post'
+					});
 				} else {
-					alert("not detected");
+					alert("Not detected");
 				}
 			});
 		},
@@ -119,74 +126,74 @@ $(function() {
 
 	handler.init();
 
-	function calculateRectFromArea(canvas, area) {
-		var canvasWidth = canvas.width,
-			canvasHeight = canvas.height,
-			top = parseInt(area.top) / 100,
-			right = parseInt(area.right) / 100,
-			bottom = parseInt(area.bottom) / 100,
-			left = parseInt(area.left) / 100;
+	// function calculateRectFromArea(canvas, area) {
+	// var canvasWidth = canvas.width,
+	// canvasHeight = canvas.height,
+	// top = parseInt(area.top) / 100,
+	// right = parseInt(area.right) / 100,
+	// bottom = parseInt(area.bottom) / 100,
+	// left = parseInt(area.left) / 100;
 
-		top *= canvasHeight;
-		right = canvasWidth - canvasWidth * right;
-		bottom = canvasHeight - canvasHeight * bottom;
-		left *= canvasWidth;
+	// top *= canvasHeight;
+	// right = canvasWidth - canvasWidth * right;
+	// bottom = canvasHeight - canvasHeight * bottom;
+	// left *= canvasWidth;
 
-		return {
-			x: left,
-			y: top,
-			width: right - left,
-			height: bottom - top
-		};
-	}
+	// return {
+	// x: left,
+	// y: top,
+	// width: right - left,
+	// height: bottom - top
+	// };
+	// }
 
-	Quagga.onProcessed(function(result) {
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-			drawingCanvas = Quagga.canvas.dom.overlay,
-			area;
+	// Quagga.onProcessed(function(result) {
+	// var drawingCtx = Quagga.canvas.ctx.overlay,
+	// drawingCanvas = Quagga.canvas.dom.overlay,
+	// area;
 
-		if (result) {
-			if (result.boxes) {
-				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-				result.boxes.filter(function(box) {
-					return box !== result.box;
-				}).forEach(function(box) {
-					Quagga.ImageDebug.drawPath(box, {
-						x: 0,
-						y: 1
-					}, drawingCtx, {
-						color: "green",
-						lineWidth: 2
-					});
-				});
-			}
+	// if (result) {
+	// if (result.boxes) {
+	// drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+	// result.boxes.filter(function(box) {
+	// return box !== result.box;
+	// }).forEach(function(box) {
+	// Quagga.ImageDebug.drawPath(box, {
+	// x: 0,
+	// y: 1
+	// }, drawingCtx, {
+	// color: "green",
+	// lineWidth: 2
+	// });
+	// });
+	// }
 
-			if (result.box) {
-				Quagga.ImageDebug.drawPath(result.box, {
-					x: 0,
-					y: 1
-				}, drawingCtx, {
-					color: "#00F",
-					lineWidth: 2
-				});
-			}
+	// if (result.box) {
+	// Quagga.ImageDebug.drawPath(result.box, {
+	// x: 0,
+	// y: 1
+	// }, drawingCtx, {
+	// color: "#00F",
+	// lineWidth: 2
+	// });
+	// }
 
-			if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(result.line, {
-					x: 'x',
-					y: 'y'
-				}, drawingCtx, {
-					color: 'red',
-					lineWidth: 3
-				});
-			}
+	// if (result.codeResult && result.codeResult.code) {
+	// Quagga.ImageDebug.drawPath(result.line, {
+	// x: 'x',
+	// y: 'y'
+	// }, drawingCtx, {
+	// color: 'red',
+	// lineWidth: 3
+	// });
+	// }
 
-			if (handler.state.inputStream.area) {
-				area = calculateRectFromArea(drawingCanvas, handler.state.inputStream.area);
-				drawingCtx.strokeStyle = "#0F0";
-				drawingCtx.strokeRect(area.x, area.y, area.width, area.height);
-			}
-		}
-	});
+	// if (handler.state.inputStream.area) {
+	// area = calculateRectFromArea(drawingCanvas, handler.state.inputStream.area);
+	// drawingCtx.strokeStyle = "#0F0";
+	// drawingCtx.strokeRect(area.x, area.y, area.width, area.height);
+	// }
+	// }
+	// });
 
 });
