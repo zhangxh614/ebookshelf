882dba7b8226f336660dc3b5b82f1f7af1552b15
nzpznk <skynnznk12@163.com>
2017-07-27 12:01:13 +0800

    small-fix: open link in new tab

--- src/javascript/upload.js
+++ src/javascript/upload.js
@@ -84,7 +84,7 @@ $(function() {
 									res['recommend'].map(function(item) {
 										return <li className="col-sm-6 col-md-4 col-lg-3">
 													<img className="other" src={item['img']}/>
-													<a className="link" href={item['link']}>{item['name']}</a>
+													<a className="link" target = "_blank" href={item['link']}>{item['name']}</a>
 												</li>
 									})
 								}
